56439544fd65c7ce4f44de78bc397261721f824d
SeanCodes2 <sean.coyan@outlook.com>
2022-09-02 07:50:06 -0500

    updated theme and layout

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,29 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+setTheme = (theme) => {
+    let currentTheme = document.getElementById('theme');
+
+    switch(theme){
+        case 'darkly':
+            currentTheme.setAttribute('href', 'css/darkly.min.css');
+            break;
+       
+        case 'simplex':
+            currentTheme.setAttribute('href', 'css/simplex.min.css');
+            break;
+        case 'sketchy':
+            currentTheme.setAttribute('href', 'css/sketchy.min.css');
+            break;
+        case 'minty':
+            currentTheme.setAttribute('href', 'css/minty.min.css');
+            break;
+            
+    }
+    localStorage.setItem('jsTheme', theme);
+}
+
+if(localStorage.getItem('jsTheme') != null){
+    setTheme(localStorage.getItem('jsTheme'));
+}
